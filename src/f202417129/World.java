a2639800b56fca395e8b800ac76287ef845be59a
Kang, Yubeen <skhu202417129@gmail.com>
2025-10-20 14:13:23 +0900

    five

--- src/f202417129/World.java
+++ src/f202417129/World.java
@@ -2,4 +2,6 @@ package f202417129;
 public class World
  { public static void run() {
    System.out.println("202417129");
-   } }
+   } 
+  public static void run2() { System.out.println("hello world"); }
+  }
